2da9ee5be08b08523c7e93f1dc9a9186418440ba
Zach <zcaspall@hotmail.com>
2022-03-03 20:09:17 -0600

    First commit

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const dictionaryModel = require("./Models/dictionaryModel");
 
 // The maximum request body size is 100 kilobytes; however, my word list was
 // ~150kb. So I just doubled the request body size limit
+app.use(express.static("public", {
+    index: "index.html",
+    extensions: ['html']
+}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json({limit: '200kb'}));
 
 let word;
